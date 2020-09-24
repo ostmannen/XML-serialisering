6744a7c7612f6b50c4a00be895c0d74e836c8ba9
Niklas <niklas.ostman02@gmail.com>
2020-09-24 15:28:30 +0200

    Skapar repository

---
+++ XML-serialisering/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Xml;
+using System.IO;
+
+namespace XML_serialisering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon sword = new Weapon();
+            sword.damage = 10f;
+            sword.reach = 2f;
+            sword.cost = 100;
+            sword.name = Console.ReadLine();
+            System.Console.WriteLine(sword.name);
+            Console.ReadLine();
+
+            XmlSerializer serializer = new XmlSerializer(typeof(Weapon));
+            FileStream file = File.Open(@"Weapon.xml", FileMode.OpenOrCreate);
+            serializer.Serialize(file, sword);
+            file.Close();
+        }
+    }
+}
---
+++ XML-serialisering/Weapon.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace XML_serialisering
+{
+    public class Weapon
+    {
+      public string name;
+      public float damage;
+      public float reach;
+      public int cost;
+    }
+}
